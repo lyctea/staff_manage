ab6fb342ff28a24ab0847208a17c53f89398fc77
lyctea <396136337@qq.com>
2016-12-14 10:11:44 +0800

    细节调整

--- src/StaffHeader.js
+++ src/StaffHeader.js
@@ -33,7 +33,7 @@ export default class StaffHeader extends React.Component {
                             <td className="headerTd"><input ref="searchBar" onChange={this.handlerSearch.bind(this)} type='text' placeholder="Search..."/></td>
                             <td className="headerTd">
                                 {/*for 属性规定 label 与哪个表单元素绑定。*/}
-                                <label for="idSelect">人员筛选</label>
+                                <label for="idSelect">人员筛选:  </label>
                                 <select id="idSelect" ref="selId" onChange={this.handlerIdChange.bind(this)}>
                                     <option value="0">全部</option>
                                     <option value="1">主任</option>
@@ -43,7 +43,7 @@ export default class StaffHeader extends React.Component {
                                 </select>
                             </td>
                             <td>
-                                <label className="orderSelect">排列方式</label>
+                                <label className="orderSelect">排列方式:  </label>
                                 <select id="orderSelect" ref="selOrder" onChange={this.handlerOrderChange.bind(this)}>
                                     <option value="0">身份</option>
                                     <option value="1">年龄升</option>
